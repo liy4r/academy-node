6294377da73a8b7017abcf749f25c078d2703bb7
liy4r <b.dulguun0114@gmail.com>
2025-11-03 20:26:23 +0800

    jisheehiiv

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-import { startGame } from "./print.js";
-
-startGame();
+import * as fs from "node:fs";
+fs.writeFile("jishee.txt","fdsafsadfdsa","utf-8",()=>{
+    
+});
+fs.readFile("jishee.txt","utf-8",(error,data)=>{
+    console.log(data);
+})
